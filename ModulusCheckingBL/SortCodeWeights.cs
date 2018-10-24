8fa66bb50230461028ac3e0ea581f184b782a5a6
Andrew@Teebs020 <andrewTeebay@gmail.com>
2018-10-24 16:34:49 +0100

    Tidy up

--- ModulusCheckingBL/SortCodeWeights.cs
+++ ModulusCheckingBL/SortCodeWeights.cs
@@ -1,11 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.IO;
-using System.Text.RegularExpressions;
+using System.Linq;
 using System.Reflection;
+using System.Text.RegularExpressions;
 using System.Web;
 
 namespace ModulusCheckingBL
